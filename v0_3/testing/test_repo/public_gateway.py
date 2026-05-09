4405a938cf03b560cfed50a79009e119bf080829
Cicciobello04 <trani.francesco04@gmail.com>
2026-05-10 01:57:59 +0200

    Integrato nuovo modello di embedding: mxbai-embed-large

--- v0_3/testing/test_repo/public_gateway.py
+++ v0_3/testing/test_repo/public_gateway.py
@@ -1,6 +1,6 @@
 from internal_fetcher import fetch_internal_resource
 
 def proxy_request(user_provided_url):
-    # L'utente può passare "http://localhost:8080/admin" o "http://169.254.169.254" (Metadata Cloud)
+    
     data = fetch_internal_resource(user_provided_url)
     return {"status": "success", "content": data}
