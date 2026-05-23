0a5cbc1fe1382dd81e348dced18bfbba4f7db428
Cicciobello04 <trani.francesco04@gmail.com>
2026-05-24 01:10:03 +0200

    Eliminati vecchi scenari di test, aggiungi scenari di test validi per la tesi

--- v0_3/testing/final_stress_test/database_connector.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-def get_user_data(user_id):
-    
-    query = f"SELECT * FROM users WHERE id = '{user_id}'"
-    print(f"DEBUG: Executing query {query}")
-    conn = sqlite3.connect('users.db')
-    return conn.execute(query).fetchall()
--- v0_3/testing/test_repo/public_gateway.py
+++
@@ -1,6 +0,0 @@
-from internal_fetcher import fetch_internal_resource
-
-def proxy_request(user_provided_url):
-    
-    data = fetch_internal_resource(user_provided_url)
-    return {"status": "success", "content": data}
